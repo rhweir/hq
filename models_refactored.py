5c56032676a336250e7a89a2e676be4df602d1a9
yaldi <roddy.weir@gmail.com>
2026-01-05 12:30:11 +0000

    updated classes for hero and boss

--- models_refactored.py
+++ models_refactored.py
@@ -1,3 +1,4 @@
+import random
 import data
 from dice import Dice
 
@@ -17,3 +18,46 @@ class Entity:
         # position variables
         self.x = x
         self.y = y
+
+
+class Monster(Entity):
+    """Standard monster, inherits everything from the Entity class"""
+
+    pass  # do nothing else
+
+
+class Boss(Entity):
+    """Monster with specific elements"""
+
+    def __init__(self, name, char_class, movement, attack, defend, hp, mp, x=0, y=0):
+        super().__init__(char_class, movement, attack, defend, hp, mp, x, y)
+        self.name = name
+
+
+class Hero(Entity):
+    """Hero with specific elements"""
+
+    def __init__(
+        self,
+        name,
+        char_class,
+        attack,
+        defend,
+        hp,
+        mp,
+        x=0,
+        y=0,
+        primary_weapon="Unarmed",
+        off_hand="Empty",
+        arm="Empty",
+        head="Empty",
+        body="Empty",
+    ):
+        super().__init__(char_class, 0, attack, defend, hp, mp, x, y)
+
+        self.name = name
+        self.primary_weapon = data.weapons.get(primary_weapon, data.weapons["Unarmed"])
+        self.off_hand = data.armour.get(off_hand, data.armour["Empty"])
+        self.arm = data.armour.get(arm, data.armour["Empty"])
+        self.head = data.armour.get(head, data.armour["Empty"])
+        self.body = data.armour.get(body, data.armour["Empty"])
